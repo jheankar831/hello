28905679995e81bcc881ffe53feb595953495d2b
jheankar831 <jheankar51@gmail.com>
2024-09-04 11:08:28 +0530

    new

--- p2.py
+++ p2.py
@@ -2,8 +2,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 data = pd.read_csv(r"D:\AI notes\Dataset\Dataset\titanic\tested.csv")
-age_survived = data[data['survived'] == 1]['age'] 
-age_not_survived = data[data['survived'] == 0]['age'] 
+age_survived = data[data['Survived'] == 1]['Age'] 
+age_not_survived = data[data['Survived'] == 0]['Age'] 
 plt.hist(age_survived, color='g', alpha=0.9, label='Survived')
 plt.hist(age_not_survived, color='k', alpha=0.5,label='Not Survived')
 plt.xlabel('Age')
